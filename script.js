93bb34cb3683b32966575983ab96ba32ed1ee828
RoanMacmillan <hibee94@gmail.com>
2022-09-16 15:44:58 +0100

    init

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+    const prevBtn = document.querySelectorAll('.prev');
+
+    const tanyaDisplay = document.getElementById('tanya');
+
+    const johnDisplay = document.getElementById('john');
+
+
+
+    function scrollFunction () {
+
+
+
+            johnDisplay.classList.toggle('scroll1');
+            tanyaDisplay.classList.toggle('scroll');
+
+    }
