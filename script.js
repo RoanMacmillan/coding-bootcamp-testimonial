6a4a062a64797c2853158aa992a6c6c452aef956
RoanMacmillan <hibee94@gmail.com>
2022-09-16 17:07:12 +0100

    added keydown event listener

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 
 
 
-    const prevBtn = document.querySelectorAll('.prev');
+    const buttons = document.querySelectorAll('.scroll-btn');
+
+
 
     const tanyaDisplay = document.getElementById('tanya');
 
     const johnDisplay = document.getElementById('john');
 
+    function scrollFunction (event) {
+
+            
+        if (event.keyCode === 37 || event.keyCode === 39) {
+
+            tanyaDisplay.classList.toggle('scrolltanya');
+
+            johnDisplay.classList.toggle('scrolljohn');
+
+        }  
+        
+        if (event.target.matches('a') || event.target.matches('img')) {
 
 
-    function scrollFunction () {
 
+            tanyaDisplay.classList.toggle('scrolltanya');
 
+            johnDisplay.classList.toggle('scrolljohn');
 
-            johnDisplay.classList.toggle('scroll1');
-            tanyaDisplay.classList.toggle('scroll');
+        }
 
     }
+
+
+    buttons.forEach((button) => {
+
+        button.addEventListener('click', scrollFunction);
+    
+    })
+
+    window.addEventListener('keydown', scrollFunction);
